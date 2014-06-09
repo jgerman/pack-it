0b0dff14041002a3ad826945c5634ab33c2e4175
Jeremy German <jeremy.german@gmail.com>
2014-06-09 18:10:48 -0400

    added tests for 'find'

--- test/pack_it/record_tree_test.clj
+++ test/pack_it/record_tree_test.clj
@@ -21,3 +21,46 @@
       (is (= (:length t-right) 45))
       (is (= (:width t-right) 5))
       )))
+
+
+; test find on a single node,
+; test a recurse left,
+; test a recurse right,
+; test a no match,
+; and we should be solid
+(deftest find-no-match-root
+  (testing "Finding a node root"
+    (let [t (mk-bin 50 50)
+          n (->> (gen-fit 51 1)
+                 (find-node t)
+                 )]
+      (is (= n nil))
+      )))
+
+
+(deftest find-root
+  (testing "Finding a node root"
+    (let [t (mk-bin 50 50)
+          n (->> (gen-fit 1 1)
+                 (find-node t)
+                 zip/node)]
+      (is (= (:length n) 50))
+      (is (= (:width n) 50)))))
+
+(deftest find-left
+  (testing "Finding a node left"
+    (let [t (-> (mk-bin 50 50) (split-node 5 5) tree-zip)
+          n (->> (gen-fit 1 1)
+                 (find-node t)
+                 zip/node)]
+      (is (= (:length n) 50))
+      (is (= (:width n) 45)))))
+
+(deftest find-right
+  (testing "Finding a node right"
+    (let [t (-> (mk-bin 50 50) (split-node 25 40) tree-zip)
+          n (->> (gen-fit 20 35)
+                 (find-node t)
+                 zip/node)]
+      (is (= (:length n) 25))
+      (is (= (:width n) 40)))))
